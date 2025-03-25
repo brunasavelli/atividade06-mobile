209fa95243d11fe77046e7011532ad494aa0fd18
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-03-25 16:11:57 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Stack3 from "./navigation/TabNavigator";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack3 />
+    </NavigationContainer>
+  );
+}
